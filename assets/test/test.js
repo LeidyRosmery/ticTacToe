bc9a39f0db46f224a91605e9ebf2bf375ed33e06
Leidy Rosmery <rosm3rita@hotmail.com>
2017-03-06 10:35:03 -0500

    mi commit del branch

---
+++ assets/test/test.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var app = requied ('../assets/js/app.js');
+
+describe("Tictactoe app test",function () {
+  it("Poder iniciar un nuevo juego", function(){
+
+  });
+  it ("Un jugador X debe poder hacer la jugada",function (){
+
+  });
+
+  it ("validar si algun jugador gano el juego",function(){
+
+  });
+
+  it ("Valida que haya empate" , function (){
+
+  });
+});
